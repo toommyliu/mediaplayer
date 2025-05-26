234fde17dea99c69074749e269c3d76690b82b63
toommyliu <86510873+toommyliu@users.noreply.github.com>
2025-05-26 08:08:15 -0700

    refactor: clean

--- src/renderer/src/components/FileBrowser.svelte
+++ src/renderer/src/components/FileBrowser.svelte
@@ -1,4 +1,4 @@
-<script lang="ts">
+<!-- <script lang="ts">
   interface FileBrowserProps {
     onFileSelect: (filePath: string) => void;
     onFolderSelect: (folderPath: string) => void;
@@ -7,7 +7,5 @@
   let { onFileSelect, onFolderSelect }: FileBrowserProps = $props();
 </script>
 
-<!-- Hidden component for now - file selection handled through electron dialog -->
 <div style="display: none;">
-  <!-- File browser modal would go here -->
-</div>
+</div> -->
