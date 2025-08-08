e4271223e5c80cc8f8a3c69cec160be30ba2815e
toommyliu <86510873+toommyliu@users.noreply.github.com>
2025-08-08 09:41:31 -0700

    fix: use constant step

--- src/renderer/src/lib/state/volume.svelte.ts
+++ src/renderer/src/lib/state/volume.svelte.ts
@@ -1,5 +1,6 @@
 import { PersistedState } from "runed";
 import { playerState } from "./player.svelte";
+import { VOLUME_STEP } from "$lib/constants";
 
 class VolumeState {
   private _value = new PersistedState<number>("volume", 1);
@@ -11,7 +12,6 @@ class VolumeState {
   }
 
   public set value(newValue: number) {
-    // Volume can be a double between 0 and 1
     this._value.current = Math.max(0, Math.min(1, newValue));
 
     if (this.value === 0) {
@@ -33,19 +33,17 @@ class VolumeState {
 
     if (playerState.videoElement) {
       playerState.videoElement.muted = this._isMuted;
-
-      // If muted, set volume to 0, otherwise set to current volume
       playerState.videoElement.volume = this._isMuted ? 0 : this._value.current;
     }
   }
 
   public decreaseTick() {
-    if (this.value > 0) this.value = Math.max(0, this.value - 0.05);
+    if (this.value > 0) this.value = Math.max(0, this.value - VOLUME_STEP);
     if (this.value === 0) this.isMuted = true;
   }
 
   public increaseTick() {
-    if (this.value < 1) this.value = Math.min(1, this.value + 0.05);
+    if (this.value < 1) this.value = Math.min(1, this.value + VOLUME_STEP);
     if (this.value > 0) this.isMuted = false;
   }
 }
