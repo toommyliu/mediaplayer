0b3d2a098574e1e09670945d54591476cf91caf0
toommyliu <86510873+toommyliu@users.noreply.github.com>
2026-03-28 16:11:45 -0700

    chore: more migration

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -6,51 +6,13 @@ consola.wrapAll()
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { ClerkProvider } from '@clerk/clerk-react'
-
-import { WindowListener } from '@/components/window-listener'
-import { ThemeProvider } from '@/components/theme-provider'
 import { TooltipProvider } from '@/components/ui/tooltip'
 import './assets/base.css'
 
-import { routeTree } from './routeTree.gen'
-
-const router = createRouter({ routeTree })
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 60 * 5,
-      refetchOnWindowFocus: false
-    }
-  }
-})
-
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router
-  }
-}
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ClerkProvider
-      publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}
-      routerPush={(to) => router.navigate({ to })}
-      routerReplace={(to) => router.navigate({ to, replace: true })}
-    >
-      <QueryClientProvider client={queryClient}>
-        <WindowListener />
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <TooltipProvider delay={0}>
-            <RouterProvider router={router} />
-          </TooltipProvider>
-        </ThemeProvider>
-        {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
-      </QueryClientProvider>
-    </ClerkProvider>
+      <TooltipProvider delay={0}>
+        <p>hello world</p>
+      </TooltipProvider>
   </StrictMode>
 )
