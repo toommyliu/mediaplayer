b417d97d522a58bf593d6a988275a8e4972e719e
toommyliu <86510873+toommyliu@users.noreply.github.com>
2026-03-29 19:54:06 -0700

    chore: update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,6 @@
-import prettier from "eslint-config-prettier";
-import { includeIgnoreFile } from "@eslint/compat";
-import js from "@eslint/js";
+import antfu from '@antfu/eslint-config'
 
-import globals from "globals";
-import { fileURLToPath } from "node:url";
-import ts from "typescript-eslint";
-
-const gitignorePath = fileURLToPath(new URL("./.gitignore", import.meta.url));
-
-export default ts.config(
-  includeIgnoreFile(gitignorePath),
-  js.configs.recommended,
-  ...ts.configs.recommended,
-  prettier,
-  {
-    languageOptions: {
-      globals: { ...globals.browser, ...globals.node }
-    },
-    rules: {
-      // typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
-      // see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
-      "no-undef": "off"
-    }
-  },
-
-);
+export default antfu({
+  formatters: true,
+  react: true,
+})
